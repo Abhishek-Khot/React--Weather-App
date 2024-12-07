a17dcb1f6dea0c01cb295a0ff987c6046d036910
Abhishek-Khot <khotabhishek15@gmail.com>
2024-12-07 12:01:33 +0530

    added new project

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.linkedin.com/in/abhishek-r-khot-b42025260/"
+        > Abhishek Khot</a
+      >, <a href="https://github.com/Abhishek-Khot">Open sourced on Github.</a> 
+    </p>
+  </div>
+  );
+}
